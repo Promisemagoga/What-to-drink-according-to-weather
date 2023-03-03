67ea83d39b00cd43b0d71d8fb58200bca8fab58b
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-03-03 14:59:30 +0200

    Added files

---
+++ index.js
@@ -0,0 +1,7 @@
+function hot(){
+    document.getElementById("weather").src = "assets/images/download (8).jfif"
+}
+
+function cold(){
+    document.getElementById("weather").src = "assets/images/download (9).jfif"
+}
